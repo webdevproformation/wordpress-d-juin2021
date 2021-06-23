161899fb9ec52e8ac44192dc2a4582a0796ee5bf
Malik H <malik.h@webdevpro.net>
2021-06-23 15:44:59 +0200

    loop

---
+++ tp-1/wp-content/themes/mon-theme/functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+function ma_fonction($object_post){
+    /*  var_dump(); */
+    return "nb commentaires : $object_post->comment_count" ;
+}
+// wordpress => librairie 
+//en PHP require()
+// 16h00 bon café @ toute suite !!
--- tp-1/wp-content/themes/mon-theme/index.php
+++ tp-1/wp-content/themes/mon-theme/index.php
@@ -33,8 +33,42 @@
         <section class="row">
             <div class="col">
                 <h1>Bienvenu dans mon site</h1>
-                
             </div>
+        </section>
+        <section class="row">
+
+            <!-- loop -->
+            <?php if( have_posts() ) : ?><!--  ?? réussit à récupérer de données ?? -->
+                <!-- si on a récupéré des données => afficher -->
+                <?php while( have_posts() ) : ?>
+                    <?php the_post() ?> <!-- données récupérées -->
+                    <div class="col-4">
+                        <article class="card">
+                            <header class="card-header">
+                                <h2><?= the_title() ?></h2>
+                                <?php edit_post_link( "Modifier" , "<div>" , "</div>" , $post ) ?>
+                                <?= ma_fonction( $post ) ?>
+                                <!-- <div><a href="http://localhost/wordpress/tp-1/wp-admin/post.php?post=107&action=edit">Modifier</a></div> -->
+                            </header>
+                            <!-- ajouter une image  l'image --> 
+                            <?php if(has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('post-thumbnail' , ["class" => "img-fluid"]) ?>
+                            <?php else : ?>
+                                <img src="http://via.placeholder.com/300x200" alt="">
+                            <?php endif ; ?>
+                            <div class="card-body">
+                                <?php the_excerpt() ?>
+                                <!--  petit résumé => méta information extrait ou les premiers mots
+                                the_content() => pendre l'intégralité du contenu de la zone de texte dans Gutenberg    
+                            --> 
+                            </div>
+                        </article>
+                    </div>
+
+                <?php endwhile ?>
+            <?php else : ?>
+                    <p>Aucune donnée pour la page demandée </p>
+            <?php endif ?>
         </section>
     </main>
     <footer class="container text-center mb-3">
