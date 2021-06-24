680a752637cf2757e0d213e7b5f44838c4267719
Malik H <malik.h@webdevpro.net>
2021-06-24 16:52:09 +0200

    menu en pied de page

--- tp-1/wp-content/themes/mon-theme/footer.php
+++ tp-1/wp-content/themes/mon-theme/footer.php
@@ -1,6 +1,11 @@
     </main>
     <footer class="container d-flex justify-content-center py-3 align-items-center">
         <p class="me-2 mb-0">&copy; Malik H - <?= date("Y") ?></p>
+        <?php $m = wp_get_nav_menu_items("Footer"); ?>
+        <?php foreach($m as $lien) : ?>
+            <?php $balise = "<a href=\"$lien->url\" class=\"mx-2\">$lien->title</a>" ?>
+            <?= $balise ?>
+        <?php endforeach ?>
         <?php get_template_part("rechercher" ) ?>
     </footer>
    
