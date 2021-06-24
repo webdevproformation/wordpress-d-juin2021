d11397d53e028270e84b2d5c65bb902edffb81a6
Malik H <malik.h@webdevpro.net>
2021-06-24 16:58:18 +0200

    template page.php

--- tp-1/wp-content/themes/mon-theme/page-nous-contacter.php
+++
@@ -1,5 +0,0 @@
-<?php get_header() ?>
-
-<p>je suis le fichier page-nous-contacter.php</p>
-
-<?php get_footer() ?>
--- tp-1/wp-content/themes/mon-theme/page.php
+++ tp-1/wp-content/themes/mon-theme/page.php
@@ -1,5 +1,15 @@
 <?php get_header() ?>
 
-<p>je suis le fichier page.php</p>
+<div class="row">
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : ?>
+            <?php the_post() ?>
+             <h1><?= the_title() ?></h1>
+             <div>
+                 <?= the_content() ?>
+             </div>
+        <?php endwhile ?>
+    <?php endif ?>
+</div>
 
 <?php get_footer() ?>
